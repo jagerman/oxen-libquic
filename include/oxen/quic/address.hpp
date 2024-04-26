5786cf8b1c1ce13dc335eec3f74fc08d5498fd93
dr7ana <danielrouhana@gmail.com>
2024-04-26 12:39:31 -0700

    Tunneling binaries
    - TCP connections can be send over a QUIC connection tunneled over datagrams now
    - I'll write more later

--- include/oxen/quic/address.hpp
+++ include/oxen/quic/address.hpp
@@ -3,6 +3,7 @@
 #include <oxenc/endian.h>
 
 #include <compare>
+#include <oxenc/bt.h>
 
 #include "formattable.hpp"
 #include "ip.hpp"
@@ -272,11 +273,17 @@ namespace oxen::quic
         // sockaddr pointer).
         void update_socklen(socklen_t len) { _addr.addrlen = len; }
 
-        std::string host() const;
+        std::string host(bool no_format = false) const;
 
         // Convenience method for debugging, etc.  This is usually called implicitly by passing the
         // Address to fmt to format it.
         std::string to_string() const;
+
+        std::string bt_encode() const;
+
+        void bt_encode(oxenc::bt_dict_producer& btdp) const;
+
+        static std::optional<Address> bt_decode(oxenc::bt_dict_consumer& btdc);
     };
 
     struct RemoteAddress : public Address
@@ -361,6 +368,12 @@ namespace oxen::quic
         Path invert() const { return {remote, local}; }
 
         std::string to_string() const;
+
+        std::string bt_encode() const;
+
+        void bt_encode(oxenc::bt_dict_producer& btdp) const;
+
+        static std::optional<Path> bt_decode(oxenc::bt_dict_consumer& btdc);
     };
 }  // namespace oxen::quic
 
