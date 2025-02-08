fe2ae1de6f4795aaadb5bff186bfbc388ee98685
Jason Rhinelander <jason@imaginary.ca>
2025-02-08 12:29:36 -0400

    Change SHAKE128 to SHAKE256
    Not for security reasons (shake128 is plenty for a reset token), but
    just library compatibility: Nettle only very recently added shake128
    support, but shake256 has been supported for years.

--- src/gnutls_crypto.cpp
+++ src/gnutls_crypto.cpp
@@ -37,13 +37,13 @@ namespace oxen::quic
     hashed_reset_token::hashed_reset_token(
             std::span<const uint8_t, NGTCP2_STATELESS_RESET_TOKENLEN> token, std::span<const uint8_t> static_secret)
     {
-        // SHAKE128(STATELESS_HASH_PREFIX || static_secret || token)
-        sha3_128_ctx ctx;
-        sha3_128_init(&ctx);
-        sha3_128_update(&ctx, STATELESS_HASH_PREFIX.size(), reinterpret_cast<const uint8_t*>(STATELESS_HASH_PREFIX.data()));
-        sha3_128_update(&ctx, static_secret.size(), static_secret.data());
-        sha3_128_update(&ctx, token.size(), token.data());
-        sha3_128_shake(&ctx, size(), data());
+        // SHAKE256(STATELESS_HASH_PREFIX || static_secret || token)
+        sha3_256_ctx ctx;
+        sha3_256_init(&ctx);
+        sha3_256_update(&ctx, STATELESS_HASH_PREFIX.size(), reinterpret_cast<const uint8_t*>(STATELESS_HASH_PREFIX.data()));
+        sha3_256_update(&ctx, static_secret.size(), static_secret.data());
+        sha3_256_update(&ctx, token.size(), token.data());
+        sha3_256_shake(&ctx, size(), data());
     }
 
 }  //  namespace oxen::quic
